e4d2f096e236eaf9de3beaedfc44bacbaaf8fcad
GideaoBitencourt <victorgideao01@gmail.com>
2026-09-19 23:28:18 -0300

    tela de login e cadastro criadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { PostSync } from './pages/post-sync/post-sync';
+import { Login } from './pages/login/login';
+import { Cadastro } from './pages/cadastro/cadastro';
 
 export const routes: Routes = [
     {path: "", redirectTo:"post-sync", pathMatch:"full"},
-    {path: "post-sync", component: PostSync}
+    {path: "post-sync", component: PostSync},
+    {path: "login", component: Login},
+    {path: "cadastro", component: Cadastro}
 ];
